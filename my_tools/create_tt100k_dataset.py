24297ba1ca2040f85535f207656f39847eb69b48
Number1Tao <number1jtao@gmail.com>
2017-08-07 00:05:31 +0800

    V2.1 create tt100k tree

--- my_tools/create_tt100k_dataset.py
+++ my_tools/create_tt100k_dataset.py
@@ -1,81 +1,94 @@
-import xml.etree.ElementTree as ET
-import pickle
+ratio=1.0#0.9
+anno_file="annotations.json"
+save_crop_nums=1
+
+
+
 import os
 from os import listdir, getcwd
-from os.path import join
+wd=getcwd()
+wd=os.path.join(wd,"data")
 
-ratio=0.9 
-classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
-
-
-def convert(size, box):
-    dw = 1./size[0]
-    dh = 1./size[1]
-    x = (box[0] + box[1])/2.0
-    y = (box[2] + box[3])/2.0
-    w = box[1] - box[0]
-    h = box[3] - box[2]
-    x = x*dw
-    w = w*dw
-    y = y*dh
-    h = h*dh
-    return (x,y,w,h)
-
-def convert_annotation(image_id):
-    in_file = open('Annotations/%s.xml'%(image_id))
-    out_file = open('labels/%s.txt'%(image_id), 'w')
-    tree=ET.parse(in_file)
-    root = tree.getroot()
-    size = root.find('size')
-    w = int(size.find('width').text)
-    h = int(size.find('height').text)
-
-    for obj in root.iter('object'):
-        difficult = obj.find('difficult').text
-        cls = obj.find('name').text
-        if cls not in classes or int(difficult) == 1:
-            continue
-        cls_id = classes.index(cls)
-        xmlbox = obj.find('bndbox')
-        b = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text), float(xmlbox.find('ymin').text), float(xmlbox.find('ymax').text))
-        bb = convert((w,h), b)
-        out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
-
-wd = getcwd()
-
-#part 1 :create train.txt test.txt
-images=os.listdir(wd+"JPEGImages") 
-train_file=open("train_set.txt",'w')
-test_file= open("test_set.txt",'w')
-all_images=[]
-for name in os.listdir(anno_dir): 
-    if name[:-3]+"jpg" in images:
-	all_images.append(name[:-4])
-
+import json
+datas=json.load(open(os.path.join(wd,anno_file),'r'))
+labels=datas["types"]
+imgs=datas["imgs"]
+count_dic={}
+examples_nums={}
+for label in labels:
+	count_dic[label]=0
+	examples_nums[label]=0
+############################################################
+	# part 1 :create train.txt test.txt
 import random
-random.shuffle(all_images)
+from PIL import Image
+indexs=list(imgs.keys())
+random.shuffle(indexs)
+train_nums=len(imgs)*ratio
+train_file=open(os.path.join(wd,"train.txt"), 'w')
+test_file=open(os.path.join(wd,"test.txt"), 'w')
 k=0
-for image in all_images:
-    if k<len(all_images)*ratio:
-        train_file.write("%s\n" % image)
-    else:
-	test_file.write("%s\n" % image)
-    k+=1
+
+if os.path.exists(os.path.join(wd,"labels"))==False:
+	os.mkdir(os.path.join(wd,"labels"))
+if os.path.exists(os.path.join(wd,"examples"))==False:
+	os.mkdir(os.path.join(wd,"examples"))
+else:
+	os.removedirs(os.path.join(wd,"examples"))
+	os.mkdir(os.path.join(wd, "examples"))
+for index  in indexs:
+	img=imgs[index]
+	_path=os.path.join(wd, img['path'])
+	if os.path.exists(_path):
+		if k<train_nums:
+			train_file.write("%s\n" % _path)
+		else:
+			test_file.write("%s\n" % _path)
+		objs=img["objects"]
+		out_file = open(os.path.join(wd,'labels/%s.txt' % (img['id'])), 'w')
+		
+		im=Image.open(_path)
+		size_w,size_h=im.size
+		for obj in objs:
+			category= obj["category"]
+			xmax	= float(obj['bbox']["xmax"])
+			ymax    = float(obj['bbox']["ymax"])
+			xmin 	= float(obj['bbox']["xmin"])
+			ymin 	= float(obj['bbox']["ymin"])
+			###########convert ############
+			dw = 1. / size_w
+			dh = 1. / size_h
+			x = (xmin + xmax) / 2.0
+			y = (ymin + ymax) / 2.0
+			w = xmax - xmin
+			h = ymax - ymin
+			x = x * dw
+			w = w * dw
+			y = y * dh
+			h = h * dh
+			out_file.write(str(labels.index(category)) + " " + str(x)+ " " + str(y)+" " + str(w)+" " + str(h)+'\n')
+			
+			count_dic[category]+=1
+			
+			if examples_nums[category]<save_crop_nums:
+				temp=im.copy()
+				temp.crop((int(xmin), int(ymin), int(xmax), int(ymax))).save(os.path.join(wd,"examples",category+".jpg"))
+				examples_nums[category]+=1
+				
+		out_file.close()
+	k+=1
 train_file.close()
 test_file.close()
 
+names=open(os.path.join(wd,"tt100k.names"), 'w')
+for label in labels:
+	names.write(label+'\n')
+names.close()
 
-image_ids = open(train_file).read().strip().split()
-list_file = open("train.txt", 'w')
-for image_id in image_ids:
-    list_file.write('%s/JPEGImages/%s.jpg\n'%(wd, image_id))
-    convert_annotation( image_id)
-    list_file.close()
 
-image_ids = open(test_file).read().strip().split()
-list_file = open("test.txt", 'w')
-for image_id in image_ids:
-    list_file.write('%s/JPEGImages/%s.jpg\n'%(wd, year, image_id))
-    convert_annotation( image_id)
-    list_file.close()
+print count_dic
+############################################################
 
+
+
+######plot image
