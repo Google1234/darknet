8adc40a5308f070e1de084b9a896dae7afc4f44e
Number1Tao <number1jtao@gmail.com>
2017-08-16 13:47:13 +0800

    V3.1 fix video bug

--- my_tools/create_vedio.py
+++ my_tools/create_vedio.py
@@ -38,19 +38,31 @@ data_root = wd+'/results/'
 
 makedirs(save_root)
 files_list=glob.glob(data_root+'*')
+files=[]
+for file in files_list:
+    base=os.path.basename(file)
+    files.append(int(os.path.splitext(base)[0]))
+
+base=os.path.basename(files_list[0])
+suffix=os.path.splitext(base)[1]
+dir_name=os.path.dirname(files_list[0])
+
+files.sort()
 # pdb.set_trace()
-num_frames=len(files_list)
+num_frames=len(files)
 	
 video_path=save_root+'rst.avi'
 fps =25
 fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-img=cv2.imread(files_list[0])
+print os.path.join(dir_name,str(files[0])+suffix)
+img=cv2.imread(os.path.join(dir_name,str(files[0])+suffix))
 videoWriter = cv2.VideoWriter(video_path, fourcc,fps,(img.shape[1], img.shape[0]))
 	# fig, axes = plt.subplots(2, 1, figsize=(16, 12))
 	# ax0, ax1 = axes.ravel()
 	
-for i in range(1,len(files_list)):
-    image=cv2.imread(files_list[i])
+for i in range(1,len(files)):
+    print os.path.join(dir_name,str(files[i])+suffix)
+    image=cv2.imread(os.path.join(dir_name,str(files[i])+suffix))
     image = cv2.resize(image, (img.shape[1], img.shape[0]))
     videoWriter.write(image)# cv2.waitKey(1)	
 videoWriter.release()
